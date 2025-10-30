3c136e4faaeaa2cac6d7200902604aff5a75a955
Bazlur Rashid <mailtobazlur@gmail.com>
2025-10-30 18:08:26 +0600

    - user dashboard created

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
+
+  layout 'client'
+
   def index
+    # TODO: Add actual data when models are ready
+    # @quotes_count = current_user.quotes.count
+    # @invoices_count = current_user.invoices.count
+    # @clients_count = current_user.clients.count
+    # @total_revenue = current_user.invoices.sum(:total_amount)
   end
 end
