ab36e25f52d68e29212fe74897b34868a20dc731
Bazlur Rashid <mailtobazlur@gmail.com>
2025-10-30 20:51:37 +0600

    - layout usability checked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,20 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  layout :determine_layout
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def determine_layout
+    # Admin controllers use admin layout
+    return 'admin' if self.class.name.start_with?('Admin::')
+    # Devise controllers use devise layout
+    return 'devise' if devise_controller?
+    # All other controllers use client layout
+    'client'
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
